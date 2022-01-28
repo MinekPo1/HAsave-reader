e743738591a0ed00900115ef24e91975359e5d8d
walker fowlkes <walkerffo22@gmail.com>
2022-01-27 16:01:25 -0800

    git-vscode forgot to push

--- classes.py
+++ classes.py
@@ -13,16 +13,19 @@ class HASave:
 		while 1==1:
 			try:
 				key, l = self.__ext_str__()
-			except IndexError:
+			except IndexError as err:
+				print(self.data)
+				print(err)
 				break
-			if l == 0: continue
+			if (l == 0): continue
+			if (l == 1): self.data.pop(0);continue
 			logging.debug(f"read key: {key}, len: {l}")
 			var_type = resolve_type(key)
 			var = None
 			logging.debug(f"type: {var_type}")
 			if var_type == "short":var = self.__ext_short__()
 			if var_type == "long":var  = self.__ext_long__()
-			if var_type == "str":var = self.__ext_str__()
+			if var_type == "str":var = self.__ext_str__()[0]
 			if var == None:raise KeyError(f"INVALID KEY: {key}")
 			logging.debug(f"value: {var}")
 			self.values[key] = var
@@ -31,6 +34,13 @@ class HASave:
 	def __ext_short__(self)->int:
 		low = f"{self.data.pop(0):b}"
 		high = f"{self.data.pop(0):b}"
+		print(f"low: {low}\nhigh: {high}")
+		while len(high) < 8:
+			high = "0"+high
+		while len(low) < 8:
+			low = "0"+low
+		print(f"low: {low}\nhigh: {high}")
+		print("short bin: ",high+low)
 		val = int(high+low,2)
 		if val >= 32768: val -= 65536
 		return val
@@ -39,16 +49,20 @@ class HASave:
 		low = f"{self.data.pop(0):b}"
 		high = f"{self.data.pop(0):b}"
 		highest = f"{self.data.pop(0):b}"
+		print("long bin: ",highest+high+low+lowest)
 		val = int(highest+high+low+lowest,2)
 		if val >= 2147483648: val -= 4294967296
 		return val
-	def __ext_str__(self)->str:
+	def __ext_str__(self) -> tuple[str, int]:
 		str_len = self.data.pop(0)
+		if str_len == 0: return "",0
 		string = ""
+		print("extracting string len:", str_len)
 		for _ in range(str_len):
 			val = self.data.pop(0)
 			if val == 0: continue
 			string += chr(val)
+		self.data.pop(0)
 		return string,str_len
 	def __repr__(self):
 		return f"<HAsave v:{self.save_version} obj#: {len(self.values)}>"
@@ -57,8 +71,10 @@ class HASave:
 
 if __name__ == "__main__":
 	logging.basicConfig(level=logging.DEBUG)
-	logging.info("testing save_data/the_inventory")
-	with open("save_data/the_inventory","rb") as save:
+	pth = "save_data/perks"
+	logging.info(f"testing {pth}")
+	with open(pth,"rb") as save:
 		ba = bytearray(save.read())
 		save = HASave(ba)
 		print(save)
+		print(save.values)
--- values.py
+++ values.py
@@ -1,12 +1,33 @@
 import re
-short_vars = ["n_stored_items"]
-short_regex = ["entry[0-9]+_count","entry[0-9]+_n_strings","entry[0-9]+_slot"]
-long_vars = ["default"]
+short_vars = [
+	"n_stored_items",
+	"player_chunk_x",
+	"player_inner_x",
+	"player_chunk_x",
+	"player_inner_y",
+	"player_inner_z"
+	]
+short_regex = [
+	"entry[0-9]+_count",
+	"entry[0-9]+_n_strings",
+	"entry[0-9]+_slot",
+	"perk_[^_]*$"
+	]
+long_vars = [
+	"default",
+	"entry1_n_strings"
+	]
 long_regex = []
-str_vars = []
-str_regex = []
+str_vars = [
+	"player_zone"
+	]
+str_regex = [
+	"entry[0-9]+_string[0-9]+_key",
+	"entry[0-9]+_string[0-9]+_value",
+	"perk_slot_."
+	]
 
-def resolve_type(name)->str:
+def resolve_type(name:str)->str|None:
 	if name in short_vars:
 		return "short"
 	if name in long_vars:
